d2203d642a793a9d0ea45820cc81edd7b9c27da0
Alex Safatli <safatli@cs.dal.ca>
2015-03-03 19:05:31 -0400

    Moved all models to abstract classes.

---
+++ Model/TwitchAbstractChannel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace TwitchSharp {
+	public abstract class TwitchAbstractChannel {
+	
+		/* Model Fields */
+		public TwitchStream Stream { get; }
+		public List<TwitchUser> Editors { get; }
+		public List<TwitchVideo> Videos { get; }
+		public List<TwitchUser> Followers { get; }
+		public List<TwitchTeam> Teams { get; }
+		public TwitchGame Game { get; }
+
+		/* Meta Fields */
+		public string Mature { get; }
+		public string Status { get; }
+		public string Language { get; }
+		public string DisplayName { get; }
+		public string Logo { get; }
+		public string Banner { get; }
+		public string ProfileBanner { get; }
+		public bool Partner { get; }
+		public string URL { get; }
+		public int Views { get; }
+		public int Follows { get; }
+		public float Delay { get; }
+
+		/* Operations */
+		public abstract void refresh();
+		public abstract bool updateStatus(string status);
+		public abstract bool updateGame(TwitchGame game);
+		public abstract bool updateDelay(int delay);
+
+	}
+}
+
---
+++ Model/TwitchAbstractVideo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TwitchSharp
+{
+	public abstract class TwitchAbstractVideo
+	{
+
+	}
+}
+
